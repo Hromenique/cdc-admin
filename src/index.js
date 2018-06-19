e4af7cd4d26d503589f7d48cc0d351e102194488
Hromenique Batista <hromenique.batista@maplink.global>
2018-06-19 09:16:09 -0300

    aula 06 atividade 01 - configuração do react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    (<Router>
+        <App>
+            <Switch>
+                <Route path="/" component={App} />
+                <Route path="/autor" />
+                <Route path="/livro" />
+            </Switch>
+        </App>        
+    </Router>),
+    document.getElementById('root'));
+
